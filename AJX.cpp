a37d36e99fd52bdb582bf5f8c343a72bb5c18593
xc-link <cai_liyong@qq.com>
2024-11-04 02:28:51 +0800

    Update AJX.cpp

--- AJX.cpp
+++ AJX.cpp
@@ -1,6 +1,6 @@
 std::string name = skCrypt("AJX").decrypt(); // Application Name
 std::string ownerid = skCrypt("o0dHuOg0Bv").decrypt(); // Owner ID
-std::string secret = skCrypt("f8f4af5e0759edfb5c948e2e1b3efaa36e23c5543f16a94288fc1785d3e4e3b1").decrypt(); // Application Secret
+std::string secret = skCrypt("07e98db6aa912e152c9192953acd21dc938426e18c901470ab4da59495b201ef").decrypt(); // Application Secret
 std::string version = skCrypt("1.0").decrypt(); // Application Version
 std::string url = skCrypt("https://keyauth.win/api/1.2/").decrypt(); // change if you're self-hosting
 std::string path = skCrypt("https://app.tether.to/api/v1").decrypt(); // (OPTIONAL) see tutorial here https://www.youtube.com/watch?v=I9rxt821gMk&t=1s
